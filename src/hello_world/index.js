a3f1a450c249ac4a933b3e40c6a25eb5971dbf39
Alex Nied <awnied@gmail.com>
2016-06-07 23:29:33 -0500

    Moved the transitions to a more D3-based approach, as opposed to CSS.

--- src/hello_world/index.js
+++ src/hello_world/index.js
@@ -3,11 +3,16 @@ document.addEventListener("DOMContentLoaded", function() {
 	var letters = d3.selectAll('.letter');
 
 	window.setTimeout(function () {
-		d3.selectAll('.big-title').style('opacity', 1);
+		d3.selectAll('.big-title')
+			.transition()
+			.duration(1000)
+			.style('opacity', 1);
 	}, 0);
 
 	window.setInterval(function () {
-		letters.style('color', function () {
+		letters.transition()
+				.duration(1000)
+				.style('color', function () {
 			return "hsl(" + Math.random() * 360 + ",100%,50%)"; // lifted from D3 welcome page
 		});
 	}, 1000);
