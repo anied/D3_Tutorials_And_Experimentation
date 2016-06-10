c685d8e0cfab0e1521445f6a540a0ebe228d59f6
Alex Nied <awnied@gmail.com>
2016-06-10 00:14:02 -0500

    Heat colors for the graph

--- src/bar_graph_1/index.js
+++ src/bar_graph_1/index.js
@@ -1,16 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-	var x = d3.scale.linear()
+	var calcHeight = d3.scale.linear()
 	    .domain([0, 100])
 	    .range([0, 500]);
 
+	var calcColor = d3.scale.linear()
+		.domain([0, 100])
+		.range([0, 255]);
 
-	var data = [10,20,30,40,50];
+	function generateRGB(datum) {
+		return 'rgb(' + calcColor(datum) + ',0,' + (255-calcColor(datum)) + ')';
+	}
+
+	var data = [20,40,60,80,100];
 
 	d3.select('.bar-graph')
-		.style('padding-top', (500 - x(d3.max(data))) + 'px')
+		.style('padding-top', (500 - calcHeight(d3.max(data))) + 'px')
 		.selectAll('div.bar')
 			.data(data)
 		.enter().append('div').classed('bar', true)
-			.style('height', function (d) { return x(d) + 'px'});
+			.style({
+				'height' : function (d) { return calcHeight(d) + 'px'},
+				'background-color' : function (d) { return generateRGB(d) }
+			});
 });
