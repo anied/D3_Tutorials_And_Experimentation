598d99e5290f5532af2c7b2c6340908f497f2c91
Alex Nied <awnied@gmail.com>
2016-06-10 00:04:08 -0500

    A little more panache around the hardcoded data

--- src/bar_graph_1/index.js
+++ src/bar_graph_1/index.js
@@ -5,9 +5,12 @@ document.addEventListener('DOMContentLoaded', function () {
 	    .range([0, 500]);
 
 
+	var data = [10,20,30,40,50];
+
 	d3.select('.bar-graph')
+		.style('padding-top', (500 - x(d3.max(data))) + 'px')
 		.selectAll('div.bar')
-			.data([10,20,30,40,50])
+			.data(data)
 		.enter().append('div').classed('bar', true)
 			.style('height', function (d) { return x(d) + 'px'});
 });
