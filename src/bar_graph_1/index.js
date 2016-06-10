4f8f97ab0d9eda20c319d421a4d3c01970c6410d
Alex Nied <awnied@gmail.com>
2016-06-10 00:40:30 -0500

    Initial loadup working but updates are not being applied.

--- src/bar_graph_1/index.js
+++ src/bar_graph_1/index.js
@@ -6,21 +6,47 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	var calcColor = d3.scale.linear()
 		.domain([0, 100])
-		.range([0, 255]);
+		.rangeRound([0, 255]);
 
-	function generateRGB(datum) {
-		return 'rgb(' + calcColor(datum) + ',0,' + (255-calcColor(datum)) + ')';
+	var inputs = document.querySelectorAll('.graph-bar-val-range');
+
+	function getUpdatedData() {
+		var data = [];
+		var i;
+
+		for (i = 0; i < inputs.length; i++) {
+			data.push(inputs[i].value);
+		}
+
+		return data;
+
 	}
 
-	var data = [20,40,60,80,100];
+	function updateGraph(data) {
 
-	d3.select('.bar-graph')
-		.style('padding-top', (500 - calcHeight(d3.max(data))) + 'px')
-		.selectAll('div.bar')
-			.data(data)
-		.enter().append('div').classed('bar', true)
-			.style({
-				'height' : function (d) { return calcHeight(d) + 'px'},
-				'background-color' : function (d) { return generateRGB(d) }
-			});
+		function generateRGB(datum) {
+			return 'rgb(' + calcColor(datum) + ',0,' + (255-calcColor(datum)) + ')';
+		}
+
+		d3.select('.bar-graph')
+			.style('padding-top', (500 - calcHeight(d3.max(data))) + 'px')
+			.selectAll('div.bar')
+				.data(data)
+			.enter().append('div').classed('bar', true)
+				.style({
+					'height' : function (d) { return calcHeight(d) + 'px'},
+					'background-color' : function (d) { 
+						console.log(generateRGB(d));
+						return generateRGB(d) 
+					}
+				});
+
+	}
+
+	document.querySelector('#update').addEventListener('click', function () {
+		var newData = getUpdatedData();
+		updateGraph(newData);
+	});
+
+
 });
