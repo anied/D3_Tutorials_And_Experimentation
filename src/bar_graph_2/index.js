5a7da97802549f7de36943ceb85500a06b4171a6
Alex Nied <awnied@gmail.com>
2016-06-11 23:27:14 -0500

    Wired up data fetches.

--- src/bar_graph_2/index.js
+++ src/bar_graph_2/index.js
@@ -7,19 +7,69 @@ document.addEventListener('DOMContentLoaded', function () {
 	var zipSubmitButton = $qs('#getWeatherByZip');
 	var geolocationAvailable = ("geolocation" in navigator);
 
-	if (!geolocationAvailable) {
+	if (geolocationAvailable) {
 		$qs('body').className += "geolocation-supported";
 	} else {
 		$qs('body').className += "geolocation-unsupported";
 	}
 
+	function loadWeatherData(data) {
+		console.log('loadWeatherData');
+		console.log(data);
+	}
+
+	function fetchWeather(lat, long) {
+		var weatherReq = new XMLHttpRequest();
+		weatherReq.onreadystatechange = function () {
+			if (weatherReq.readyState === XMLHttpRequest.DONE) {
+			    if (weatherReq.status === 200) {
+			    	loadWeatherData(JSON.parse(weatherReq.responseText));
+				}
+				// handle other states
+			} else {
+			    // handle else
+			}
+		};
+		weatherReq.open('GET', 'http://forecast.weather.gov/MapClick.php?lat=' + lat + '&lon=' + long + '&FcstType=json', true);
+		weatherReq.send();
+	}
+
+	function fetchCoords(zip) {
+		var coordReq = new XMLHttpRequest();
+		coordReq.onreadystatechange = function () {
+			var location;
+			if (coordReq.readyState === XMLHttpRequest.DONE) {
+			    if (coordReq.status === 200) {
+				    location = JSON.parse(coordReq.responseText).results[0].geometry.location;
+				    fetchWeather(location.lat, location.lng);
+				}
+				// handle other states
+			} else {
+			    // handle else
+			}
+		};
+		coordReq.open('GET', 'http://maps.googleapis.com/maps/api/geocode/json?address=' + zip, true);
+		coordReq.send();
+	}
+
+	function getWeatherByLocation() {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			fetchWeather(position.coords.latitude, position.coords.longitude);
+		});
+	}
+
+	function getWeatherByZip() {
+		var zipcode = $qs('#zipcode').value;
+		fetchCoords(zipcode);
+	}
+
 	locationButton
 		.addEventListener('click', function () {
-			console.log('GET WEATHER BY LOCATION');
+			getWeatherByLocation();
 		});
 
 	zipSubmitButton
 		.addEventListener('click', function () {
-			console.log('GET WEATHER BY ZIP');
+			getWeatherByZip();
 		});
 });
