58c9938000d0cbf58d2313f0aa6d3ac67ff7d1e7
Alex Nied <awnied@gmail.com>
2016-06-15 00:45:12 -0500

    Got the update working-- man that is not as clear as it ought to be.... Code is maybe a little weird bc it was a lot of tweaking to get it working-- may come back and make it more sane, but setting a commit here for safe-keeping...

--- src/joins_101/index.js
+++ src/joins_101/index.js
@@ -15,37 +15,53 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	function sectionTwo() {
 		var data = [1, 1, 2, 3, 5, 8, 13];
-		var paragraphs = d3.select('#sectionTwo').selectAll('p')
-							.data(data);
 
-		function updateReadout(newData) {
-			var readoutMsg = '['+newData.toString().replace(/,/g, ', ')+']';
-			document.querySelector('#lessonTwo .current-data').value = readoutMsg;
-		}
-
-		function updateData() {
-			var data = [];
+		function updateData(newData) {
+			var data = newData || [];
 			var i;
-			var numDataPoints = Math.ceil(Math.random()*10); //Data can contain between 1 and 10 values
+			var numDataPoints;
+			var paragraphs;
 
-			for (i = 0; i < numDataPoints.length; i++) {
-				data.push(Math.round(Math.random()*100)); //Data is any integer between 0 and 100
+			function updateReadout(newData) {
+				var readoutMsg = '['+newData.toString().replace(/,/g, ', ')+']';
+				document.querySelector('#lessonTwo .current-data').value = readoutMsg;
 			}
 
-			paragraphs.data(data);
-			updateReadout(data);
+			if (data.length < 1) {
 
+				numDataPoints = Math.ceil(Math.random()*10); //Data can contain between 1 and 10 values
+
+				for (i = 0; i < numDataPoints; i++) {
+					data.push(Math.round(Math.random()*100)); //Data is any integer between 0 and 100
+				}
+
+			}
+
+			paragraphs = d3.select('#sectionTwo').selectAll('p')
+								.data(data);
+
+			paragraphs.enter()
+					.append('p')
+					.text(function (d, i) { return 'Paragraph #'+i+' -- Data: '+d; });
+
+			paragraphs.exit().remove();
+
+			paragraphs.text(function (d, i) { return 'Paragraph #'+i+' -- Data: '+d; });
+
+			updateReadout(data);
+			
 		}
 
-		paragraphs.enter()
-				.append('p')
-				.text(function (d, i) {
-					return 'Paragraph #'+i+' -- Data: '+d;
-				});
 
-		updateReadout(data);
+		updateData(data);
+
+		document.querySelector('#lessonTwo .request-fresh-data').addEventListener('click', function () {
+			updateData();
+		});
 
 	}
+
+
 
 	sectionOne();
 	sectionTwo();
