32dd5073f326026514dce01b28194727839e7b61
Alex Nied <awnied@gmail.com>
2016-07-16 23:06:53 -0500

    Yeesh.... not working... but the closest I've gotten thus far so keeping a revision point...

--- src/bar_graph_3/index.js
+++ src/bar_graph_3/index.js
@@ -8,9 +8,10 @@ document.addEventListener('DOMContentLoaded', function () {
 	var zipInput = $qs('#zipcode');
 	var geolocationAvailable = ("geolocation" in navigator);
 
-	var height = 400;
+	var margin = {top: 20, right: 30, bottom: 30, left: 40};
+	var height = 450 - margin.top - margin.bottom;
+	var width = 770 - margin.left - margin.right;
 	var barHeightMAX = 300;
-	var width = 700;
 	var maxTemp = 100;
 
 	var calcBarHeight = d3.scale.linear()
@@ -24,6 +25,21 @@ document.addEventListener('DOMContentLoaded', function () {
 	var x = d3.scale.ordinal()
 					.rangeBands([0, width], 0.1);
 
+	var xAxis = d3.svg.axis()
+						.scale(x)
+						.orient('bottom');
+
+	var yAxis = d3.svg.axis()
+						.scale(calcBarHeight)
+						.orient('left');
+
+
+	var barGraph = d3.select('.bar-graph')
+						.attr('height', height + margin.top + margin.bottom)
+						.attr('width', width + margin.left + margin.right)
+							.append('g')
+							.attr('transform', 'translate('+margin.left+','+margin.top+')');
+
 
 	function loadWeatherData(data) {
 		//TODO-- everything goes in the one big method to start-- later on it ought to be properly broken out into separate functions for setup, update, etc...
@@ -35,11 +51,6 @@ document.addEventListener('DOMContentLoaded', function () {
 		var tempsArr = data.data.temperature;
 		var dataArr = [];
 		var i;
-
-
-		var barGraph = d3.select('.bar-graph')
-							.attr('height', height)
-							.attr('width', width);
 
 		var bar;
 
@@ -53,13 +64,25 @@ document.addEventListener('DOMContentLoaded', function () {
 				return d.label;
 			}));
 
-			bar = barGraph.selectAll('g')
+
+			barGraph.append("g")
+			    .attr("class", "x axis")
+			    .attr("transform", "translate(0," + height + ")")
+			    .call(xAxis);
+
+			barGraph.append("g")
+			    .attr("class", "y axis")
+			    .call(yAxis);
+
+
+			bar = barGraph.selectAll('g.bar-wrap')
 					.data(dataArr);
 
 			bar.exit().remove();
 
 			bar.enter()
 					.append('g')
+					.attr('class', '.bar-wrap')
 					.attr('transform', function(d, i) {
 						return 'translate(' + x(d.label) + ',' + (barHeightMAX-calcBarHeight(d.temp)) + ')';
 					})
@@ -111,7 +134,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	function zeroData(callback) {
 
-		var bar = d3.select('.bar-graph').selectAll('g');
+		var bar = d3.select('.bar-graph').selectAll('.bar-wrap');
 
 		var barLength = bar[0].length;
 		var n = 0;
