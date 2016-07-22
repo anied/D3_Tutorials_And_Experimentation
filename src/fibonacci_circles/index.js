3d4549c6e6a0cd5e5eabfe5908e5da82278b1a1f
Alex Nied <awnied@gmail.com>
2016-07-22 17:11:19 -0500

    Wired up fibonacci generator.

--- src/fibonacci_circles/index.js
+++ src/fibonacci_circles/index.js
@@ -1,16 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
 	
+	var height;
+	var width;
+	var svg = document.querySelector('#fibonacci-circles');
+	var data = [];
+
 	function calculateSVGDimensions() {
 		//TODO-- there may be a better way to handle this...
 		var topSection = document.querySelector('#introAndControlsWrapper');
 		var svgWrapper = document.querySelector('#fibonacciCirclesWrapper');
-		var svg = document.querySelector('#fibonacci-circles');
 
-		svg.style.height = window.innerHeight - topSection.offsetHeight;
-		svg.style.width = svgWrapper.getBoundingClientRect().width;
+		height = window.innerHeight - topSection.offsetHeight;
+		width = svgWrapper.getBoundingClientRect().width;
+
+		svg.style.height = height;
+		svg.style.width = width;
 	}
 
+	function incrementData() {
 
+		var length = data.length;
+
+		if ( length === 0 ) {
+			data.push(1);
+		} else if ( length === 1 ) {
+			data.push(1);
+		} else {
+			data.push(data[length-1]+data[length-2]);
+		}
+	}
+
+	function registerEventListeners() {
+		document.querySelector('#incrementButton').addEventListener('click', incrementData);
+	}
+
+	registerEventListeners();
 	calculateSVGDimensions();
 
 
