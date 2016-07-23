24fe8890d82eb1561a3a46ed8dfbdb7bbcc01f25
Alex Nied <awnied@gmail.com>
2016-07-23 13:50:40 -0500

    Added 'how selections work' section and got the setup for exercise one in place-- next is to actually write it...

---
+++ src/how_selections_work/index.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+	function arrayToDisplayString(inputArr) { //writing for the moment as though I can assume that the array will only ever contain numbers
+		return '[ ' + inputArr.toString().replace(new RegExp(',', 'g'), ', ') + ' ]';
+	}
+
+	function generateArbitraryData() {
+		var outputArr = [];
+		var i;
+
+		for (i=0; i<5; i++) {
+			outputArr.push(Math.round(Math.random()*100));
+		}
+
+		return outputArr;
+	}
+
+	function lessonOneSectionOne() {
+		var data = generateArbitraryData();
+		d3.select('#lessonOne .section-one .rendering-area .arbitrary-data-label')
+		  .text(arrayToDisplayString(data));
+
+
+	}
+
+	lessonOneSectionOne();
+
+});
